533ced8d405550a4d70830d4b4e5d7fe14ed0117
Caleb <caleb.tutty@nzherald.co.nz>
2015-02-25 13:50:39 +1300

    Remove root static folder

--- static/admin/js/user_admin.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-  $("#childNode").detach().appendTo("#parentNode");
-  $('#documentsetformentry_set-group table td input[type=hidden]').detach().appendTo('form');
-  $('#documentsetformentry_set-group table th:first-child, #documentsetformentry_set-group table td:first-child, #documentsetformentry_set-group table td:nth-child(2)').remove();
-});
